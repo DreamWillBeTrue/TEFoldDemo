b19471ec120224f5e8285df2035a8c7b913c2691
徐瑞栋 <tudou13934858460@163.com>
2020-01-19 14:17:40 +0800

    随意做的修改

--- TEFoldDemo/ViewController.m
+++ TEFoldDemo/ViewController.m
@@ -3,7 +3,7 @@
 //  TEFoldDemo
 //
 //  Created by offcn_c on 16/6/22.
-//  Copyright © 2016年 offcn_c. All rights reserved.
+//  Copyright © 2016年 offcn_c. All rights reserved. 
 //
 
 #import "ViewController.h"
